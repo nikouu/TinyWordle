e654f1468b7d4c468cc7159acb93d8db107b8941
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-03-26 17:48:31 +1300

    Added first 11 attempts

---
+++ 07-attempt-7/GameUI.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+
+namespace TinyWordle
+{
+    public class GameUI
+    {
+        public void DisplayGame(GuessedWord[] guessedWords)
+        {
+            Console.Clear();
+            Console.WriteLine("TinyWordle");
+
+            foreach (GuessedWord guessedWord in guessedWords)
+            {
+                if (string.IsNullOrEmpty(guessedWord.Word))
+                {
+                    Console.WriteLine("#####");
+                }
+                else
+                {
+                    var line = new StringBuilder();
+                    foreach (var guessedLetter in guessedWord.GuessedLetters)
+                    {
+                        if (guessedLetter.IsCorrect)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(guessedLetter.Letter);
+                            Console.ResetColor();
+                        }
+                        else if (guessedLetter.IsRightLetterWrongPlace)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkYellow;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(guessedLetter.Letter);
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(guessedLetter.Letter);
+                            Console.ResetColor();
+                        }
+                    }
+
+                    Console.Write("\r\n");
+                }
+            }
+        }
+
+        public void DisplayInvalidInput()
+        {
+            Console.WriteLine("Not a valid input.");
+        }
+
+        public void DisplayWonGame()
+        {
+            Console.WriteLine("You win!");
+        }
+
+        public void DisplayLostGame()
+        {
+            Console.WriteLine("You lose!");
+        }
+
+        public void ContinueScreen()
+        {
+            Console.WriteLine("Hit any key to play again. Hit 'q' to quit.");
+        }
+    }
+}
