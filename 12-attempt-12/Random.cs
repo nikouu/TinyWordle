f9112acfeb8e41944cde26fde5871d6123227079
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-03-26 20:19:33 +1300

    Start of attempt 12

---
+++ 12-attempt-12/Random.cs
@@ -0,0 +1,15 @@
+﻿namespace TinyWordle
+{
+    // https://github.com/MichalStrehovsky/SeeSharpSnake/blob/master/Game/Random.cs
+    struct Random
+    {
+        private uint _val;
+
+        public Random(uint seed)
+        {
+            _val = seed;
+        }
+
+        public uint Next() => _val = (1103515245 * _val + 12345) % 2147483648;
+    }
+}
