76bef7c07a380c08c2937b48ba2ab505bbbb487b
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-03-28 15:40:02 +1300

    Attempt 13

---
+++ 13-attempt-13/GameManager.cs
@@ -0,0 +1,85 @@
+﻿namespace TinyWordle
+{
+    public class GameManager
+    {
+        private string[] _wordList;
+        private GameUI _gameUI;
+        private Random _random;
+
+        public GameManager(string[] wordList)
+        {
+            _wordList = wordList;
+            _gameUI = new GameUI();
+            _random = new Random((uint)Environment.TickCount64);
+        }
+
+        public void Menu()
+        {
+            while (true)
+            {
+                GameLoop();
+                _gameUI.ContinueScreen();
+
+                var shouldContinue = Console.ReadLine();
+
+                if (shouldContinue == "q")
+                {
+                    return;
+                }
+            }
+        }
+
+        private void GameLoop()
+        {
+            var index = _random.Next() % _wordList.Length;
+            var word = _wordList[index];
+
+            var game = new Game(word);
+
+            while (true)
+            {
+                _gameUI.DisplayGame(game.GuessedWords);
+
+                var guessedWord = Console.ReadLine();
+
+                if (!IsValidGuess(guessedWord))
+                {
+                    continue;
+                }
+
+                var result = game.Guess(guessedWord);
+
+                if (result == State.Playing)
+                {
+                    continue;
+                }
+                else if (result == State.Won)
+                {
+                    _gameUI.DisplayGame(game.GuessedWords);
+                    _gameUI.DisplayWonGame();
+                    break;
+                }
+                else if (result == State.Lost)
+                {
+                    _gameUI.DisplayGame(game.GuessedWords);
+                    _gameUI.DisplayLostGame();
+                    break;
+                }
+            }
+        }
+
+        private bool IsValidGuess(string guess)
+        {
+            if (guess == null)
+            {
+                return false;
+            }
+            else if (guess.Length != 5)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 13-attempt-13/GameUI.cs
@@ -0,0 +1,58 @@
+﻿namespace TinyWordle
+{
+    public class GameUI
+    {
+        public void DisplayGame(GuessedWord[] guessedWords)
+        {
+            Console.Clear();
+            Console.Write("TinyWordle\r\n");
+
+            foreach (GuessedWord guessedWord in guessedWords)
+            {
+                if (string.IsNullOrEmpty(guessedWord.Word))
+                {
+                    Console.Write("#####\r\n");
+                }
+                else
+                {
+                    foreach (var guessedLetter in guessedWord.GuessedLetters)
+                    {
+                        if (guessedLetter.IsCorrect)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                            Console.Write(guessedLetter.Letter);
+                        }
+                        else if (guessedLetter.IsRightLetterWrongPlace)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkYellow;
+                            Console.Write(guessedLetter.Letter);
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            Console.Write(guessedLetter.Letter);                            
+                        }
+                    }
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write("\r\n");
+                }
+            }
+        }
+
+        public void DisplayWonGame()
+        {
+            Console.Write("\r\nYou win!");
+        }
+
+        public void DisplayLostGame()
+        {
+            Console.Write("\r\nYou lose!");
+        }
+
+        public void ContinueScreen()
+        {
+            Console.Write("\r\nHit any key to play again. Hit 'q' to quit.");
+        }
+    }
+}
