163827bbffc962a2d780fc27875feaa6e5130228
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-03-28 23:16:38 +1300

    Turns out the non records structs are smaller according to just the dll

--- 13-attempt-13/GuessedLetter.cs
+++ 13-attempt-13/GuessedLetter.cs
@@ -1,4 +1,9 @@
 ﻿namespace TinyWordle
 {
-    public readonly record struct GuessedLetter(char Letter, bool IsRightLetterWrongPlace, bool IsCorrect);
+    public struct GuessedLetter
+    {
+        public char Letter;
+        public bool IsRightLetterWrongPlace;
+        public bool IsCorrect;
+    }
 }
