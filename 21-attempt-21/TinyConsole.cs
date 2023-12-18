23da1dde841a32f85d8cfa847f29afb9d5b99969
Niko <983351+nikouu@users.noreply.github.com>
2023-12-18 18:11:00 +1300

    Update TinyConsole.cs

--- 21-attempt-21/TinyConsole.cs
+++ 21-attempt-21/TinyConsole.cs
@@ -5,24 +5,24 @@ namespace TinyWordle
 {
     // https://stackoverflow.com/a/75958239
     // A rough look at using p/invoke to replace Console
-public static class TinyConsole
-{
-    [DllImport("msvcr120.dll")]
-    public static extern int printf(string format);
-
-    [DllImport("msvcr120.dll")]
-    public static extern int system(string command);
-
-    [DllImport("msvcr120.dll")]
-    private static extern IntPtr gets(StringBuilder value);
-
-    public static string ReadLine()
+    public static class TinyConsole
     {
-        var value = new StringBuilder();
+        [DllImport("msvcr120.dll")]
+        public static extern int printf(string format);
 
-        gets(value);
+        [DllImport("msvcr120.dll")]
+        public static extern int system(string command);
 
-        return value.ToString();
+        [DllImport("msvcr120.dll")]
+        private static extern IntPtr gets(StringBuilder value);
+
+        public static string ReadLine()
+        {
+            var value = new StringBuilder();
+
+            gets(value);
+
+            return value.ToString();
+        }
     }
-}
 }
